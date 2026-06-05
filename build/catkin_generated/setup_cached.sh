68ce3c6d45e19a5aac4153bfa4b003bc545c0811
KR10x <krishnas24@iitk.ac.in>
2026-06-05 05:49:21 +0530

    fixed the bug

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/kr10x_linux/Downloads/biped_walking_controller/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/kr10x_linux/Downloads/biped_walking_controller/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/kr10x_linux/Downloads/biped_walking_controller/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/kr10x_linux/Downloads/biped_walking_controller/src:$ROS_PACKAGE_PATH"
---
+++ build/chronos_balance/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kr10x_linux/Downloads/biped_walking_controller/src/chronos_balance
+# Build directory: /home/kr10x_linux/Downloads/biped_walking_controller/build/chronos_balance
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
